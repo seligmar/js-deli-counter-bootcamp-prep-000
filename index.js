a26588cf4ecdd0a783a0e15fd19171428a102a29
Mary Selig <seligmar@gmail.com>
2019-01-15 17:58:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,5 @@ function currentLine(line) {
    if (line.length === 0) { return "The line is currently empty."; } 
    for (var i = 0; i < line.length; i++) {
     katzDeli.push(`${i + 1}. ${line[i]},`); }
-    // while (i < line.length); }
   return `The line is currently:" + ${katzDeli}`;   
 } 
